33f7062cae2bae75f8a9ac378e08efc117b6b439
euaallee <alexanderjoshua2004@gmail.com>
2024-04-24 20:24:58 -0300

    Lista

---
+++ userData.js
@@ -0,0 +1,52 @@
+const userData = [
+    {
+        user: "Alex",
+        pwd: 4004,
+        nome: "Alexander Joshua",
+        ano: 2004,
+        mes: 5,
+        dia: 22
+    },
+    {
+        user: "Admin",
+        pwd: 2,
+        nome: "Administrador",
+        ano: 2005,
+        mes: 12,
+        dia: 20
+    },
+    {
+        user: "Fulano",
+        pwd: 4884,
+        nome: "Fulano de Lima",
+        ano: 1984,
+        mes: 12,
+        dia: 19
+    },
+    {
+        user: "Joshua",
+        pwd: 1111,
+        nome: "Joshua",
+        ano: 2004,
+        mes: 5,
+        dia: 22
+    },
+    {
+        user: "Xinbinha",
+        pwd: 6006,
+        nome: "João",
+        ano: 2006,
+        mes: 6,
+        dia: 28
+    },
+    {
+        user: "jp",
+        pwd: 6776,
+        nome: "Pereira",
+        ano: 1976,
+        mes: 2,
+        dia: 21
+    },
+];
+
+export default userData;
